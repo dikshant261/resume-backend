f65e8bb52f65a01d04bbe03171003b80bfca493b
Dikshant Kumar <dhimandk261@gmail.com>
2025-03-13 02:24:58 +0530

    updated file

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: (origin, callback) => {
       const allowedOrigins = [
-        "http://localhost:5173",
+        "https://resume-backend-83nf.onrender.com",
       ]; // List of allowed origins
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
